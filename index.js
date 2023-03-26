0f02f4427a246440259556b6763c23164a75b862
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-03-26 16:24:49 +0530

    mobiles-Atlas

--- index.js
+++ index.js
@@ -127,30 +127,26 @@ app.use("/users", userRouter);
 
 // https://localhost:4000/mobiles
 
-app.get("/mobiles", function (request, response) {
-  const mobiles = [
-    {
-      "model": "OnePlus 9 5G",
-      "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-      "company": "Oneplus"
-    },
-    {
-      "model": "Iphone 13 mini",
-      "img": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-      "company": "Apple"
-    },
-    {
-      "model": "Samsung s21 ultra",
-      "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-      "company": "Samsung"
-    },
-    {
-      "model": "Xiomi mi 11",
-      "img": "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-      "company": "Xiomi"
-    }
-  ]
+app.get("/mobiles", async function (request, response) {
+
+  const mobiles = await client
+                .db('newMongo')
+                .collection('mobiles')
+                .find({})
+                .toArray();
+
   response.send(mobiles)
+})
+
+app.post("/mobiles", async function(request, response) {
+  const data = request.body;
+
+  const result = await client
+                .db('newMongo')
+                .collection('mobiles')
+                .insertMany(data);
+  
+  response.send(result)
 })
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 export const auth = (request, response, next) => {
     try{
         const token = request.header("x-auth-token")
-        console.log(token)
+        // console.log(token)
         jwt.verify(token, process.env.SECRET_KEY)
         next();
     }
